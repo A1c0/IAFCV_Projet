71c623ffd355a73fa1d60d3f014b31431285f55b
Alexandre CONTASSOT <alexandrecontassot@hotmail.fr>
2018-11-12 15:01:20 +0100

    Ajout du main

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ from src.NeuralNetwork_td import NeuralNetwork
 import numpy as np
 
 def main():
+    data_manager = DataManager()
+    data_manager.loadData()
+    neural_network = NeuralNetwork()
+    neural_network.create_model()
+    neural_network.train(data_manager.train_data, data_manager.train_labels, 6)
+    result = neural_network.evaluate(data_manager.eval_data, data_manager.eval_labels)
+    print("Accuracy : {}".format(result))
+
     pass
 
 if __name__ == "__main__":
